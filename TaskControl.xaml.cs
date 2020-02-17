d438ab3f0e8c322fd0889e97a1860ea0e22f2cb5
Jasuse <61157367+Jasuse@users.noreply.github.com>
2020-02-17 19:33:57 +0300

    Add files via upload

---
+++ TaskControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestMakerWPF_FirstInstance
+{
+    /// <summary>
+    /// Interaction logic for TaskControl.xaml
+    /// </summary>
+    public partial class TaskControl : Window
+    {
+        public TaskControl()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
